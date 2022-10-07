2222010eaa59ed98fe414aed07b949f5d4a9e32f
Taha <syedtaha.hsn@gmail.com>
2022-10-07 22:54:51 +0500

    modified:   frontend/src/store/index.js

--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -3,7 +3,7 @@ import {configureStore, createSlice} from '@reduxjs/toolkit'
 
 const authSlice = createSlice({
     name: "auth",
-    initialState: {isLoggedIn:false},
+    initialState: {isLoggedIn:true},
     reducers: {
         login(state) {
             state.isLoggedIn = true
